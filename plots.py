016049d8eff754cc67e081b98efc60e7c9aea54d
Klinton <xemiph11@gmail.com>
2024-02-24 16:56:37 -0500

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,3 +1,6 @@
-import streamlit as st
-
-st.write("Hello world")
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+df = pd.read_csv("Rides.csv", sep=",", header=0)
+plt.hist(df["member_casual"])
+plt.show()
